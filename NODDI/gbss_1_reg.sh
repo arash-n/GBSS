75ec7b955abb2f4f3f4bfa436c2f6a7fd8e6f94c
Aras <arashnazeri@Arashs-MacBook-Pro-2.local>
2015-07-08 12:41:55 -0400

    updated

--- NODDI/gbss_1_reg.sh
+++ NODDI/gbss_1_reg.sh
@@ -1,123 +1,170 @@
 
-###v2.0 26/03/2015
+#!/bin/bash
+
+#v2.1 July 28th 2015
+#Arash Nazeri, Kimel Family Translational
+#Imaging-Genetics Research Lab
+#This script depends on ANTs v2.1 and FSL v4.1.9 (or higher)
+
+#Developed at Kimel Family Translational Imaging Genetics Ressearch
+#Laboratory (TIGR), Research Imaging Centre, Campbell Family Mental
+#Health Institute,Centre for Addiction and Mental Health (CAMH),
+#Toronto, ON, Canada
+# http://imaging-genetics.camh.ca
+
+
+usage() {
+echo ""
+echo "This script nonlinearly registers diffusion images to a template space."
+echo "Outputs from the NODDI and DTI models should be already available."
+echo ""
+echo "This script works as follows:"
+echo "1) The input older containing the following subdirectories:"
+echo "FA, CSF, ODI, fIC"
+echo "2) Each Folder should contain corresponding image files with the same subject name in all folders."
+echo "Here is its usage:"
+echo "Usage: gbss_1_reg.sh input_directory [options]"
+echo ""
+echo "    -c:  to create or use a pre-existing template (default: 1, creates a template"
+echo "         0: use pre-existing template)"
+echo "    -t:  input template file here. This will be used as the initial template or"
+echo "         final template depending on -c option input."
+echo "    -h:  prints this message"
+
+echo ""
+exit 1
+}
+
+[ "$1" = "" ] && usage
+
+#Setting Defaults
+method=1;
+template="${FSLDIR}/MNI152_T1_1mm.nii.gz"
+
+while getopts "c:h:t" OPT
+
+do
+
+case $OPT in
+
+h) #help
+
+usage
+
+;;
+
+c) #threshold method
+
+method=$OPTARG
+
+if [[ ${#method} == 0 ]] ; then
+
+echo "Using a pre-existing template file..."
+
+fi
+
+;;
+
+t) # getopts issues an error message
+
+template=$OPTARG
+
+;;
+
+
+\?) # getopts issues an error message
+
+usage
+
+exit 1
+
+;;
+
+esac
+
+done
+
 
 ###############################################################
-######### PART 1.1: Making Directories/Copying Files ##########
+################# PART 1.1: Making Directories ################
 ###############################################################
 
 #making output directory/subdirectories for the analysis
 
-out_dir=/scratch/arash/NODDI/allPsych #output directory should be defined here
-mkdir ${out_dir}/
-mkdir ${out_dir}/FA
-mkdir ${out_dir}/FA/nonlinear_reg
-mkdir ${out_dir}/CSF
-mkdir ${out_dir}/ODI
-mkdir ${out_dir}/fIC
+out_dir=$1 #output directory
 mkdir ${out_dir}/tmpsapce
-
-sub_dirs=/scratch/arash/NODDI/allPsych/subdirs #The list of subject directories (in full path)
-
-#Copying files to the output directories (from the list of subject directories). CAUTION: Only the last 4 characters in the folder names will be used as the subject IDs.
-
-out_dir=/projects/arash/NODDI/NODDI-G/newPsych
-
-while read a; do
-
-string=`basename $a`
-subid=`echo ${string:(-4)}` #The number of characters can be modified here.
-
-fslmaths ${a}/DTI_FA.nii.gz -mul ${a}/brain_mask ${out_dir}/FA/${subid}.nii.gz ;
- 
-cp ${a}/*${string}_fiso.nii.gz ${out_dir}/CSF/${subid}.nii.gz ;
-cp ${a}/*${string}_odi.nii.gz ${out_dir}/ODI/${subid}.nii.gz ;
-cp ${a}/*${string}_ficvf.nii.gz ${out_dir}/fIC/${subid}.nii.gz ;
-
-echo "$string copied"
-
-done<${sub_dirs}
-
-#OPTIONAL: If there is an additional selection criteria to restrict analysis
-#Provide only the subject ids (sub_incl) that need to be included in the analysis
-
-cd ${out_dir}/FA
-
-mkdir included
-while read line
-do
-
-cp ${line}.nii.gz included
-
-done<sub_incl
-mkdir all
-mv *nii.gz all/
-mv included/*gz ./
 
 ###############################################################
 ################## PART 1.2: tbss_1_preproc ###################
 ###############################################################
 
-#Starting TBSS preproc to discard the high intensity rim of the FA files.
-#out_dir=/scratch/arash/NODDI/allPsych
+echo "Starting TBSS preproc to discard the high intensity rim of the FA files..."
 
+cd ${out_dir}/FA
 tbss_1_preproc *nii.gz
 
 ###############################################################
 ### PART 1.3: GM/WM PVE estimation/Creating PseudoT1 Images ###
 ###############################################################
+echo "GM/WM PVE estimation/Creating PseudoT1 Images..."
 
-out_dir=/scratch/arash/NODDI/allPsych
 cd ${out_dir}/FA/FA
 
 #GM probability map is created by subtracting WM and CSF probabilities maps form 1.
-module load FSL/5.0.6
-module load ANTS/2.1.0
-for a in ????_FA.nii.gz 
+for a in *_FA.nii.gz
 do
 
-fslmaths ${a} -bin ${a:0:4}_mask
+subname=`echo ${a%_*}`
+fslmaths ${a} -bin ${subname}_mask
 
-Atropos -d 3 -a ${a} -x  ${a:0:4}_mask.nii.gz -c[5,1.e-5] -i Kmeans[2] -o [segmentation.nii.gz, ${a:0:4}_%02d.nii.gz]
+Atropos -d 3 -a ${a} -x  ${subname}_mask.nii.gz -c[5,1.e-5] -i Kmeans[2] -o [segmentation.nii.gz, ${subname}_%02d.nii.gz]
 
-fslmaths  ${a:0:4}_02 ${a:0:4}_WM_frac
+fslmaths  ${subname}_02 ${subname}_WM_frac
 
-fslmaths ${a:0:4}_mask -mul ${out_dir}/ODI/${a:0:4}.nii ${out_dir}/ODI/${a:0:4}_m.nii
-fslmaths ${a:0:4}_mask -mul ${out_dir}/fIC/${a:0:4}.nii ${out_dir}/fIC/${a:0:4}_m.nii
+fslmaths ${subname}_mask -mul ${out_dir}/ODI/${subname}.nii ${out_dir}/ODI/${subname}_m.nii
+fslmaths ${subname}_mask -mul ${out_dir}/fIC/${subname}.nii ${out_dir}/fIC/${subname}_m.nii
 
-fslmaths ${a:0:4}_WM_frac -add ../../CSF/${a:0:4} -sub 1 -mul -1 -thr 0 -mul ${a:0:4}_mask  ${a:0:4}_GM_frac 
+fslmaths ${subname}_WM_frac -add ../../CSF/${subname} -sub 1 -mul -1 -thr 0 -mul ${subname}_mask  ${subname}_GM_frac 
 
-fslmaths ${a:0:4}_WM_frac -mul 2 ${a:0:4}_WM_con
-fslmaths ${a:0:4}_GM_frac -mul 1 ${a:0:4}_GM_con
-fslmaths ../../CSF/${a:0:4} -mul 0 -add ${a:0:4}_GM_con -add ${a:0:4}_WM_con ${a:0:4}_psuedoT1 #400
+fslmaths ${subname}_WM_frac -mul 2 ${subname}_WM_con
+fslmaths ${subname}_GM_frac -mul 1 ${subname}_GM_con
+fslmaths ../../CSF/${subname} -mul 0 -add ${subname}_GM_con -add ${subname}_WM_con ${subname}_psuedoT1
 
 done
 
 ###############################################################
 ### PART 1.4.a: Creating Template/ Estimating Warp Fields  ####
 ###############################################################
-#OPTION 1: Generates Template image
-
-cd ${out_dir}/FA/FA
 mkdir ../D1
-cp ????_psuedoT1.nii.gz ../D1/
+cp *_psuedoT1.nii.gz ../D1/
 cd ../D1
-buildtemplateparallel.sh  -d 3 -j 1 -o D1_ -n 0 -s MI -i 8 -m 30x50x20 -t GR -z /projects/arash/NODDI/NODDI-G/newPsych/FA/D1_template.nii.gz  *_psuedoT1.nii.gz
-#buildtemplateparallel.sh -c 1 -j 1 -d 3 -o D1_ -n 0 -s MI -i 8 -m 30x50x20 -t GR -z /scratch/arash/NODDI_PD/GBSS/FA/D1_template.nii.gz  p0*.nii.gz
-#buildtemplateparallel.sh  -c 0 -d 3 -j 1 -o D1_ -n 0 -s MI -i 8 -m 30x50x20 -t GR -z /scratch/arash/NODDI/allPsych/FA/D1_series/D1_template.nii.gz  *_psuedoT1.nii.gz
+
+if [ $method == 1 ]
+then
+
+echo "OPTION 1: Creating Template Image"
+
+buildtemplateparallel.sh  -d 3 -j 1 -o D1_ -n 0 -s MI -i 8 -m 30x50x20 -t GR -z $template  *_psuedoT1.nii.gz
+
+fi
 
 ###############################################################
 #### PART 1.4.b: Nonlinear Transformation to the Template  ####
 ###############################################################
 #OPTION 2: Requires pre-estimated Template image
+if [ $method == 0 ]
+then
+echo "OPTION 0: Nonlinear Transformation to a Pre-existing Template Image"
 
 for a in *_psuedoT1.nii.gz
 do
-label=`imglob ${a}`
+label=`${a%_*}`
 
 fsl_sub -N ANTS_${label} antsIntroduction.sh -d 3 -i ${a} -o ${label} -n 0 -s MI -m 30x50x20 -t GR -r /scratch/arash/NODDI/allPsych/FA/D1_series/D1_template.nii.gz  *_psuedoT1.nii.gz
 
 done
 
+fi
 ###############################################################
 ######### PART 1.5: Applying Warp Fields to Images  ###########
 ###############################################################
