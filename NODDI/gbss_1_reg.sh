4a401d1ca9a39e8dc07ec9cbd8ba0719560e128e
Jon Pipitone <jon@pipitone.ca>
2015-07-15 17:19:46 -0400

    Added CC by-nc license

--- NODDI/gbss_1_reg.sh
+++ NODDI/gbss_1_reg.sh
@@ -4,12 +4,21 @@
 #Arash Nazeri and Jon Pipitone, Kimel Family Translational
 #Imaging-Genetics Research Lab
 #This script depends on ANTs v2.1 and FSL v4.1.9 (or higher)
-
+#
 #Developed at Kimel Family Translational Imaging Genetics Ressearch
 #Laboratory (TIGR), Research Imaging Centre, Campbell Family Mental
 #Health Institute,Centre for Addiction and Mental Health (CAMH),
 #Toronto, ON, Canada
 # http://imaging-genetics.camh.ca
+#
+# NODDI-GBSS (c) by Arash Nazeri and Jon Pipitone, Kimel Family Translational
+# Imaging-Genetics Research Lab
+#
+# NODDI-GBSS is licensed under a
+# Creative Commons Attribution-NonCommercial 4.0 International License.
+#
+# You should have received a copy of the license along with this
+# work.  If not, see <http://creativecommons.org/licenses/by-nc/4.0/>.
 
 
 usage() {
