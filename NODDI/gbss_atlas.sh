b8a324bfbbac00b991ef2c2661258aa2b76beeaf
Arash Nazeri <arashnazeri@gmail.com>
2016-02-21 15:37:43 -0500

    Update gbss_atlas.sh

--- NODDI/gbss_atlas.sh
+++ NODDI/gbss_atlas.sh
@@ -40,7 +40,7 @@ mkdir $out_dir/atlas
 cd $out_dir/atlas
 
 ### 1. MNI to Template
-cp $FSLDIR/data/standard/MNI152_T1_1mm_brain.nii.gz $out_dir/MNI.nii.gz
+cp $FSLDIR/data/standard/MNI152_T1_1mm_brain.nii.gz $out_dir/atlas/MNI.nii.gz
 
 cp $FSLDIR/data/atlases/HarvardOxford/HarvardOxford-sub-maxprob-thr25-1mm.nii.gz ./
 cp $FSLDIR/data/atlases/HarvardOxford/HarvardOxford-cort-maxprob-thr25-1mm.nii.gz ./
@@ -49,6 +49,7 @@ cp $FSLDIR/data/atlases/MNI/MNI-maxprob-thr25-1mm.nii.gz ./
 cp $out_dir/FA/D1/D1_template.nii.gz $out_dir/atlas
 
 ANTS 3 -m MI[D1_template.nii.gz,MNI.nii.gz,1,32] -o MNI_in_temp -i 30x90x20 -r Gauss[3,1] -t Elast[3]
+
 
 if [ ! "other_atlas" = "" ]
 then
