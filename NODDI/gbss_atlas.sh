c3e5a8de74190342a95236cb5444395285f4c3a3
Arash Nazeri <arashnazeri@gmail.com>
2016-02-21 16:37:22 -0500

    Update gbss_atlas.sh

--- NODDI/gbss_atlas.sh
+++ NODDI/gbss_atlas.sh
@@ -35,7 +35,7 @@ out_dir=$1
 thr=$2
 other_atlas=$3
 
-[ "thr" = "" ] && usage
+[ "$thr" = "" ] && usage
 
 
 mkdir $out_dir/atlas
@@ -54,7 +54,7 @@ cp $out_dir/FA/D1/D1_template.nii.gz $out_dir/atlas
 ANTS 3 -m MI[D1_template.nii.gz,MNI.nii.gz,1,32] -o MNI_in_temp -i 30x90x20 -r Gauss[3,1] -t Elast[3]
 
 
-if [ ! "other_atlas" = "" ]
+if [ ! "${other_atlas}" = "" ]
 then
 cp $other_atlas $out_dir/atlas
 
@@ -73,31 +73,30 @@ WarpImageMultiTransform 3 MNI-maxprob-thr25-1mm.nii.gz atlas_MNI_in_temp_space.n
 
 ### 2. Template to MNI 
 
+
 cd ${out_dir}/FA/FA
 
-for a in ????_FA.nii.gz
+for a in *_FA.nii.gz
 
 do 
+subname=`echo ${a%_*}`
 
-WarpImageMultiTransform 3 ${out_dir}/ROI/atlas_${name}_in_temp_space.nii.gz ${a:0:4}_${aal}.nii.gz  --use-NN -R ${a} -i ${out_dir}/FA/D1/D1_${a:0:4}_psuedoT1Affine.txt ${out_dir}/FA/D1_intro/${a:0:4}_psuedoT1InverseWarp.nii.gz
+echo "Creating ROIs for ${subname} ...."
 
-#/projects/arash/home/ANTs/bin/WarpImageMultiTransformWarpImageMultiTransform 3 ../../ROIS/atlasMNI_in_temp_space.nii.gz ${a:0:4}_MNI.nii.gz  --use-NN -R ${a} -i ../D1/D1_${a:0:4}_psuedoT1Affine.txt  ${out_dir}/FA/D1_intro/${a:0:4}_psuedoT1InverseWarp.nii.gz
+WarpImageMultiTransform 3 ${out_dir}/atlas/atlas_MNI_in_temp_space.nii.gz ${subname}_MNI.nii.gz  --use-NN -R ${a} -i ${out_dir}/FA/D1/D1_${subname}_psuedoT1Affine.txt ${out_dir}/FA/D1/D1_${subname}_psuedoT1InverseWarp.nii.gz
 
-/projects/arash/home/ANTs/bin/WarpImageMultiTransform 3 ${out_dir}/ROI/cortHOx_in_temp_space.nii.gz ${a:0:4}_cHOX.nii.gz  --use-NN -R ${a} -i ${out_dir}/FA/D1_intro/${a:0:4}_psuedoT1Affine.txt  ${out_dir}/FA/D1_intro/${a:0:4}_psuedoT1InverseWarp.nii.gz
+WarpImageMultiTransform 3 ${out_dir}/atlas/atlas_cortHOx_in_temp_space.nii.gz ${subname}_cHOX.nii.gz  --use-NN -R ${a} -i ${out_dir}/FA/D1/D1_${subname}_psuedoT1Affine.txt ${out_dir}/FA/D1/D1_${subname}_psuedoT1InverseWarp.nii.gz
 
-/projects/arash/home/ANTs/bin/WarpImageMultiTransform 3 ${out_dir}/ROI/subHOx_in_temp_space.nii.gz ${a:0:4}_sHOX.nii.gz  --use-NN -R ${a} -i ${out_dir}/FA/D1_intro/${a:0:4}_psuedoT1Affine.txt  ${out_dir}/FA/D1_intro/${a:0:4}_psuedoT1InverseWarp.nii.gz
+WarpImageMultiTransform 3 ${out_dir}/atlas/atlas_subHOx_in_temp_space.nii.gz ${subname}_sHOX.nii.gz  --use-NN -R ${a} -i ${out_dir}/FA/D1/D1_${subname}_psuedoT1Affine.txt ${out_dir}/FA/D1/D1_${subname}_psuedoT1InverseWarp.nii.gz
 
-#fslmaths ${a:0:4}_GM_frac -thr ${thr} -bin -mul ${a:0:4}_aal.nii.gz ${a:0:4}_aal_${thr}.nii.gz
+if [ ! "${other_atlas}" = "" ]
+then
+WarpImageMultiTransform 3 ${out_dir}/atlas/atlas_${name}_in_temp_space.nii.gz ${subname}_${name}.nii.gz  --use-NN -R ${a} -i ${out_dir}/FA/D1/D1_${subname}_psuedoT1Affine.txt ${out_dir}/FA/D1/D1_${subname}_psuedoT1InverseWarp.nii.gz
+fslmaths ${a:0:4}_GM_frac -thr ${thr} -bin -mul ${a:0:4}_${name}.nii.gz ${a:0:4}_${name}_${thr}.nii.gz
+fi
 
-#fslmaths ${a:0:4}_GM_frac -thr ${thr} -bin -mul ${a:0:4}_MNI.nii.gz ${a:0:4}_MNI_${thr}.nii.gz
+fslmaths ${a:0:4}_GM_frac -thr ${thr} -bin -mul ${a:0:4}_MNI.nii.gz ${a:0:4}_MNI_${thr}.nii.gz
 fslmaths ${a:0:4}_GM_frac -thr ${thr} -bin -mul ${a:0:4}_cHOX.nii.gz ${a:0:4}_cHOX_${thr}.nii.gz
 fslmaths ${a:0:4}_GM_frac -thr ${thr} -bin -mul ${a:0:4}_sHOX.nii.gz ${a:0:4}_sHOX_${thr}.nii.gz
 
 done
-
-
-ANTS 3 -m MI[MNI.nii.gz,D1_template.nii.gz,1,32] -o temp_in_MNI -i 30x90x20 -r Gauss[3,1] -t Elast[3]
-
-WarpImageMultiTransform 3 D1_template.nii.gz template_MNI_warped.nii.gz -R MNI.nii.gz temp_in_MNIWarp.nii.gz temp_in_MNIAffine.txt
-
-WarpImageMultiTransform 3 all_ODI_tfce_corrp_tstat1.nii.gz MNI_ODI_tfce_corrp_tstat1.nii.gz --use-NN -R ../../ROIS/MNI.nii.gz ../../ROIS/temp_in_MNIWarp.nii.gz ../../ROIS/temp_in_MNIAffine.txt
