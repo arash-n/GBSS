67396627c665830123f144ed857e5311cf843716
Arash Nazeri <arashnazeri@gmail.com>
2016-02-23 18:40:40 -0500

    Create gbss_stats.sh

---
+++ NODDI/gbss_stats.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#v0.1 Feb 21st 2016
+#Arash Nazeri, Jon Pipitone, and Tina Roostaei, Kimel Family Translational
+#Imaging-Genetics Research Lab
+#This script depends on ANTs v2.1 and FSL v4.1.9 (or higher)
+#
+#Developed at Kimel Family Translational Imaging Genetics Ressearch
+#Laboratory (TIGR), Research Imaging Centre, Campbell Family Mental
+#Health Institute,Centre for Addiction and Mental Health (CAMH),
+#Toronto, ON, Canada
+# http://imaging-genetics.camh.ca
+#
+# NODDI-GBSS (c) by Arash Nazeri, Jon Pipitone, and Tina Roostaei
+# Kimel Family Translational Imaging-Genetics Research Lab
+#
+# NODDI-GBSS is licensed under a
+# Creative Commons Attribution-NonCommercial 4.0 International License.
+#
+# You should have received a copy of the license along with this
+# work.  If not, see <http://creativecommons.org/licenses/by-nc/4.0/>.
