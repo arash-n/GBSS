c83b923057c8aa884954aac611d11db4c62adb33
Arash Nazeri <arashnazeri@gmail.com>
2016-03-17 11:40:13 -0400

    Update preproc_script.sh

--- NODDI/native/preproc_script.sh
+++ NODDI/native/preproc_script.sh
@@ -9,8 +9,8 @@ do
 subname=`imglob $a`;
 
 fslmaths ../FA/${subname}_FA -bin ../${subname}_mask;
-Atropos -d 3 -a ../FA/${subname}_FA.nii.gz -x  ../${subname}_mask.nii.gz -i Kme$
+Atropos -d 3 -a ${a} -x  ../${subname}_mask.nii.gz -i Kmeans[2] -m [${mrf},1x1x1] -o [../segmentation.nii.gz,../${subname}_%02d.nii.gz]
 
-fslmaths ${subname}_mask -sub ../FW/$subname -sub ${subname}_02 -thr 0 ../GM/$subname
+fslmaths ../${subname}_mask -sub ../../FW/$subname -sub ../${subname}_02 -thr 0 ../../GM/$subname
 
 done
